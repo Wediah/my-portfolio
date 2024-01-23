b86e76ae55481608bbc60a1aab67da12193db3f0
Wediah <100675006+Wediah@users.noreply.github.com>
2024-01-23 23:08:19 +0000

    Update footer styling and add logo

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -5,14 +5,20 @@ import logo2 from '/public/logo2.svg'
 function Footer() {
   return (
     <div className='container mx-auto px-4 md:px-16 h-full'>
-        <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+        <hr class="h-px my-4 bg-gray-200 border-0 dark:bg-gray-700"></hr>
 
         <div className='flex justify-between items-center h-full'>
             <span>
                 ©2024 Emmanuel Wediah. All rights reserved. <br />
-                Source code: <a href="https://github.com/Wediah/my-portfolio">/github.com/Wediah/my-portfolio</a>
+                <p>
+                    Source code: 
+                    <a href="https://github.com/Wediah/my-portfolio">
+                        /github.com/Wediah/my-portfolio
+                    </a>
+                    </p>
             </span>
 
+            <Image src={logo2} alt="logo" width={50} height={50} />
         </div>
     </div>
   )
