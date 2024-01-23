3e0d85c06e798b5c177b1674def522373ca70655
Wediah <100675006+Wediah@users.noreply.github.com>
2024-01-23 22:21:31 +0000

    Update navbar component

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,29 +1,40 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
+
+
 
 function Navbar() {
   return (
     <>
-      <div className="w-full h-20 bg-black text-white">
+      <div className="w-full h-20 bg-transparent">
         <div className="container mx-auto px-4 h-full">
           <div className="flex justify-between items-center h-full">
            
+            
+           
             <ul className="hidden md:flex gap-x-6 text-white">
-              <li>
+                <li>
+                    <Link href="/">
+                        <p>Home</p>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/about">
+                        <p>About</p>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/contact">
+                        <p>Contact</p>
+                    </Link>
+                </li>
+                {/* <li>
                 <Link href="/about">
                   <p>About Us</p>
                 </Link>
-              </li>
-              <li>
-                <Link href="/services">
-                  <p>Services</p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/contacts">
-                  <p>Contacts</p>
-                </Link>
-              </li>
+              </li> */}
+              
             </ul>
             
           </div>
