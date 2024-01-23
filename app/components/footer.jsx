4df714aa2baa78defe30a7aff462cdfd8b718337
Wediah <100675006+Wediah@users.noreply.github.com>
2024-01-23 23:11:54 +0000

    Update footer component styling

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -4,13 +4,13 @@ import logo2 from '/public/logo2.svg'
 
 function Footer() {
   return (
-    <div className='container mx-auto px-4 md:px-16 h-full'>
+    <div className='container mx-auto px-4 md:px-16 h-full pb-4'>
         <hr class="h-px my-4 bg-gray-200 border-0 dark:bg-gray-700"></hr>
 
         <div className='flex justify-between items-center h-full'>
             <span>
                 ©2024 Emmanuel Wediah. All rights reserved. <br />
-                <p>
+                <p className='text-xs font-thin font-mono'>
                     Source code: 
                     <a href="https://github.com/Wediah/my-portfolio">
                         /github.com/Wediah/my-portfolio
