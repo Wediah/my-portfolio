4f9fc539ca8e85e84eaf408a78494b10284d9fdc
Wediah <100675006+Wediah@users.noreply.github.com>
2024-01-25 23:41:57 +0000

    Refactor navbar component to highlight active page

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,19 +1,23 @@
+'use client'
+
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import logo from '/public/logo.svg'
-
+import { usePathname } from 'next/navigation'
 
 
 function Navbar() {
+  const pathname = usePathname()
+
   return (
     <>
       <div className="w-full z-20 h-20 bg-transparent sticky">
         <div className="container mx-auto px-4 md:px-16 h-full">
           <div className="flex justify-between items-center h-full">
             <ul className="hidden md:flex gap-x-4 font-medium text-black dark:text-white relative z-0">
-                <li className='active:bg-gray-200 focus:bg-gray-200 hover:bg-gray-200 hover:text-black p-1 rounded-md flex-1'>
-                    <Link href="/page">
+                <li className='hover:bg-gray-200 hover:text-black p-1 rounded-md flex-1'>
+                    <Link className={`link ${pathname === '/' ? 'active' : 'bg-gray-200'}`} href="/">
                         <p>Home</p>
                     </Link>
                 </li>
