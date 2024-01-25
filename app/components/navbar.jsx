ffc460828a07db010a9cc2a2d4927f01862262b1
Wediah <100675006+Wediah@users.noreply.github.com>
2024-01-25 23:22:58 +0000

    Update navbar styles

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import logo from '/public/logo.svg'
-import { useRouter } from 'next/router'
+
 
 
 function Navbar() {
@@ -12,22 +12,22 @@ function Navbar() {
         <div className="container mx-auto px-4 md:px-16 h-full">
           <div className="flex justify-between items-center h-full">
             <ul className="hidden md:flex gap-x-4 font-medium text-black dark:text-white relative z-0">
-                <li className='active:bg-gray-200 focus:bg-gray-200 hover:bg-gray-200 p-1 rounded-md flex-1'>
+                <li className='active:bg-gray-200 focus:bg-gray-200 hover:bg-gray-200 hover:text-black p-1 rounded-md flex-1'>
                     <Link href="/">
                         <p>Home</p>
                     </Link>
                 </li>
-                <li className='active:bg-gray-200 focus:bg-gray-200 hover:bg-gray-200 p-1 rounded-md flex-1'>
+                <li className='active:bg-gray-200 focus:bg-gray-200 hover:bg-gray-200 hover:text-black p-1 rounded-md flex-1'>
                     <Link href="/about">
                         <p>About</p>
                     </Link>
                 </li>
-                <li className='active:bg-gray-200 focus:bg-gray-200 hover:bg-gray-200 p-1 rounded-md flex-1'>
+                <li className='active:bg-gray-200 focus:bg-gray-200 hover:bg-gray-200 hover:text-black p-1 rounded-md flex-1'>
                     <Link href="/projects">
                         <p>Projects</p>
                     </Link>
                 </li>
-                <li className='active:bg-gray-200 focus:bg-gray-200 hover:bg-gray-200 p-1 rounded-md flex-1'>
+                <li className='active:bg-gray-200 focus:bg-gray-200 hover:bg-gray-200 hover:text-black p-1 rounded-md flex-1'>
                     <Link href="/blog">
                         <p>Blog</p>
                     </Link>
