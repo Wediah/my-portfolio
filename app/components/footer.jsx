4f5795b87c3b43894f7dd8e573eade559fe5335e
Wediah <100675006+Wediah@users.noreply.github.com>
2024-01-26 01:10:24 +0000

    Update footer component and package dependencies

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -1,23 +1,22 @@
 import React from 'react'
 import Image from 'next/image'
 import logo2 from '/public/logo2.svg'
+import { LuGithub } from "react-icons/lu";
 
 function Footer() {
   return (
     <div className='container mx-auto px-4 md:px-16 h-full pb-4'>
-        <hr class="h-px my-4 bg-black border-0 dark:bg-white"></hr>
+        <hr class="h-px my-4 bg-black border-0 dark:bg-white decoration-wavy"></hr>
 
         <div className='flex justify-between items-center h-full'>
         <Image src={logo2} alt="logo" width={50} height={50} />
 
             <span className=' text-md text-black dark:text-white'>
                 ©2024 Emmanuel Wediah. All rights reserved. <br />
-                <p className='text-sm font-mono text-right'>
-                    Source code: 
-                    <a className='underline pl-1' href="https://github.com/Wediah/my-portfolio">
-                        /github.com/Wediah/my-portfolio
-                    </a>
-                    </p>
+                <p className='text-sm font-mono right-0  items-center flex gap-1'>
+                    Source code
+                    <LuGithub />
+                </p>
             </span>
 
             
