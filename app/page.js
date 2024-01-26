79ed88edb7ef218580059a903549dd9d36ac5d34
Wediah <100675006+Wediah@users.noreply.github.com>
2024-01-26 11:04:08 +0000

    Add empty div to main container

--- app/page.js
+++ app/page.js
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen  ">
-     
+      <div>
+        
+      </div>
     </main>
   );
 }
