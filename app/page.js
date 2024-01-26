51e9cf395f9d808f195e2e671316e8c2df61fd87
Wediah <100675006+Wediah@users.noreply.github.com>
2024-01-26 11:35:02 +0000

    Add welcome button and introduction text

--- app/page.js
+++ app/page.js
@@ -1,10 +1,17 @@
 import Image from "next/image";
+import { PiHandWavingLight } from "react-icons/pi";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen  ">
       <div>
-        
+        <button className="flex items-center gap-1 bg-green-600 rounded-md p-1">
+          <PiHandWavingLight className="text-white"/> Welcome to my website. 
+        </button>
+        <p>
+          I'm Wediah Emmanuel. A Software Engineer from Ghana. <br />
+          On this website, you'll find my projects, blog posts and more.
+        </p>
       </div>
     </main>
   );
