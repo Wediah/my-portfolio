9cab8d0003736f21b91b95a6319a97530b6fde7d
Wediah <100675006+Wediah@users.noreply.github.com>
2024-01-27 22:46:50 +0000

    Add personal information to about page

--- app/about/page.js
+++ app/about/page.js
@@ -3,7 +3,12 @@ import React from 'react'
 function page() {
   return (
     <div className="min-h-screen px-4 md:px-16 container mx-auto">
-      
+      <p>
+        Get to know me
+      </p>
+      <p>
+        Hello, I'm Emmanuel Wediah, a final year student at <a href='https://atu.edu.gh/'>Accra Technical Universtity</a> studing Computer Science. Currently, my main focus is frontend web development as well as backend development with Laravel and PHP. <br/> I hope to learn more about artificial intelligence and machine learning in the future. Aside from programming, I enjoy photography and editing postcasts.  
+      </p>
     </div>
   )
 }
