67ee8c495ee3960761ce13abdeafd7c2121bbd70
Wediah <100675006+Wediah@users.noreply.github.com>
2024-01-27 23:14:51 +0000

    Update about page content

--- app/about/page.js
+++ app/about/page.js
@@ -3,12 +3,14 @@ import React from 'react'
 function page() {
   return (
     <div className="min-h-screen px-4 md:px-32 container mx-auto">
-      <p>
+      <div className='md:pt-40 pt-32'>
+      <p className='font-medium dark:text-gray-400 text-gray-200'>
         Get to know me
       </p>
-      <p>
+      <p className='pt-5'>
         Hello, I'm Emmanuel Wediah, a final year student at <a href='https://atu.edu.gh/'>Accra Technical Universtity</a> studing Computer Science. Currently, my main focus is frontend web development as well as backend development with Laravel and PHP. <br/> I hope to learn more about artificial intelligence and machine learning in the future. Aside from programming, I enjoy photography and editing postcasts.  
       </p>
+      </div>
     </div>
   )
 }
