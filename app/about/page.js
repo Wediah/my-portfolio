34610c26b32390ef0a744e758261c1fcbd1c7a60
Wediah <100675006+Wediah@users.noreply.github.com>
2024-01-27 23:39:38 +0000

    Update about page content

--- app/about/page.js
+++ app/about/page.js
@@ -7,9 +7,9 @@ function page() {
       <p className='font-medium  text-orange-500'>
         Get to know me
       </p>
-      <p className='pt-5'>
-        Hello, I'm Emmanuel Wediah, a final year student at <a href='https://atu.edu.gh/'>Accra Technical Universtity</a> studing Computer Science. Currently, my main focus is frontend web development as well as backend development with Laravel and PHP.
-        <br/> I'm also a member of the <a href='https://gdsc.community.dev/accra-technical-university/'>Google Developers Student Club (GDSC)</a> at my school, where I help organize events and workshops for students to learn about Google technologies.
+      <p className='pt-5 w-9/12'>
+        Hello, I'm Emmanuel Wediah, a final year student at <a className='text-orange-500' href='https://atu.edu.gh/'>Accra Technical Universtity</a> studing Computer Science. Currently, my main focus is frontend web development as well as backend development with Laravel and PHP. <br/>
+        <br/> I'm also a member of the <a className='text-orange-500' href='https://gdsc.community.dev/accra-technical-university/'>Google Developers Student Club (GDSC)</a> at my school, where I help organize events and workshops for students to learn about Google technologies.<br/>
          <br/> I hope to learn more about artificial intelligence and machine learning in the future. Aside from programming, I enjoy photography and editing postcasts.  
       </p>
       </div>
