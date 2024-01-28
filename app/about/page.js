ae10c19dd5029ca2d2cbe07e4d76d88746e06ca0
Wediah <100675006+Wediah@users.noreply.github.com>
2024-01-28 00:17:21 +0000

    Update experiences in about page

--- app/about/page.js
+++ app/about/page.js
@@ -19,21 +19,19 @@ function page() {
       <div className='pt-10 w-9/12'>
         <h1 className='font-medium text-gray-200 dark:text-gray-500'>My experiences</h1>
         
-<ol class="relative border-s border-gray-200 dark:border-gray-700">                  
+<ol class="relative border-s border-gray-200 dark:border-gray-700 mt-5">                  
     <li class="mb-10 ms-4">
         <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-        <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">February 2022</time>
-        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Application UI code in Tailwind CSS</h3>
-        <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p>
-        <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">Learn more <svg class="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-  </svg></a>
+        <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Nov. 2023 - Present</time>
+        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Freelance Frontend Developer </h3>
+        <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Helping brands and individuals enhance their online presence by developing user-friendly and visual appealing websites. </p>
+        
     </li>
     <li class="mb-10 ms-4">
         <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-        <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">March 2022</time>
-        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Marketing UI design in Figma</h3>
-        <p class="text-base font-normal text-gray-500 dark:text-gray-400">All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p>
+        <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Oct. 2023 - Nov. 2023</time>
+        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Backend Engineer Intern - <a className='text-orange-500' href='https://www.linkedin.com/company/itspoynt/'>Poynt Technologies Limited</a></h3>
+        <p class="text-base font-normal text-gray-500 dark:text-gray-400">At Poynt, I became involved  </p>
     </li>
     <li class="ms-4">
         <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
