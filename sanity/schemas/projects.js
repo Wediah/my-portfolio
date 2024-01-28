31d3af0f22647b5cf853c032b409fd39ad646fc3
Wediah <100675006+Wediah@users.noreply.github.com>
2024-01-28 23:44:34 +0000

    Add projects schema to sanity schema

---
+++ sanity/schemas/projects.js
@@ -0,0 +1,30 @@
+export default {
+    name: "projects",
+    title: "Projects",
+    type: "document",
+    fields: [
+        {
+        name: "title",
+        title: "Project Title",
+        type: "string",
+        },
+        {
+        name: "description",
+        title: "Project Description",
+        type: "text",
+        },
+        {
+        name: "link",
+        title: "Project Link",
+        type: "url",
+        },
+        {
+        name: "image",
+        title: "Project Image",
+        type: "image",
+        options: {
+            hotspot: true,
+        },
+        },
+    ],
+}
