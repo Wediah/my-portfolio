911afb81b1406ebb02b539355091f8bfde59fb4a
Wediah <100675006+Wediah@users.noreply.github.com>
2024-01-28 22:19:59 +0000

    Update page heading to reflect projects worked on

--- app/projects/page.js
+++ app/projects/page.js
@@ -5,7 +5,7 @@ function page() {
     <div className="min-h-screen px-4 md:px-32 container mx-auto">
         <div className='md:pt-32 pt-28'>
             <h1 className='font-medium  text-gray-400 dark:text-gray-500'>
-            Get to know me
+                Projects I've worked on
             </h1>
         </div>
     </div>
