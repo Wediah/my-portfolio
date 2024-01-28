b3269f350126c8977f8349b623d06d1899161f69
Wediah <100675006+Wediah@users.noreply.github.com>
2024-01-28 22:05:30 +0000

    Refactor variable names for clarity

---
+++ app/blog/page.js
@@ -0,0 +1,13 @@
+import react from "react";
+
+
+export default function Blog() {
+  return (
+    <main className="flex min-h-screen px-4 md:px-32 container mx-auto ">
+      <div className="md:pt-32 pt-28">
+        <button className="flex items-center gap-1 bg-green-600 rounded-md p-1 pb-1"></button>
+        <h1>No articles yet, but what this space.</h1>
+      </div>
+    </main>
+  );
+}
