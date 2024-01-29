0e8b181d7442c926bab1337a388667d33b87c611
Wediah <100675006+Wediah@users.noreply.github.com>
2024-01-29 17:36:31 +0000

    Fix import statement and update environment variables

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,6 +1,6 @@
 'use client'
 import React, { useEffect, useState } from 'react'
-import { createClient } from 'next-sanity'
+
 
 function page({ projects }) {
   return (
--- sanity/env.ts
+++ sanity/env.ts
@@ -3,12 +3,12 @@ export const apiVersion =
 
 export const dataset = assertValue(
   process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
+  'production'
 )
 
 export const projectId = assertValue(
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
+  'qe0yd7qt'
 )
 
 export const useCdn = false
