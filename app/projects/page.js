f9936ea7545e2d95913642e6baf3341356dbb7cc
Wediah <100675006+Wediah@users.noreply.github.com>
2024-01-31 01:22:03 +0000

    Remove commented out code in page.js

--- app/projects/page.js
+++ app/projects/page.js
@@ -20,7 +20,7 @@ async function page() {
                 Projects I've worked on
             </h1>
 
-            {/* <div className=''>
+            <div className=''>
                 {projects.map((project, index) => (
                     <div className='md:py-16 py-10' key={index}>
                         <div className='md:flex md:justify-between'>
@@ -41,7 +41,7 @@ async function page() {
                         </div>
                     </div>
                 ))}
-            </div> */}
+            </div>
         </div>
     </div>
   )
