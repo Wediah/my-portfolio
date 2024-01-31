462e9c89c26eefc14a8795149e551fef4bd9f6ae
Wediah <100675006+Wediah@users.noreply.github.com>
2024-01-31 01:33:54 +0000

    Add getAllProjects function to fetch all projects from Sanity

--- sanity/sanity.query.ts
+++ sanity/sanity.query.ts
@@ -1,2 +1,14 @@
 import { groq } from 'next-sanity';
 import { client }  from './lib/client';
+
+export async function getAllProjects() {
+  return client.fetch(
+    groq`
+    *[_type == "project"]{ 
+        title, 
+        description, 
+        link, 
+        image
+    }`
+    );
+}
