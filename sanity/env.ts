b9eefc407b0185d7ffc9b54ea4fee1d8ce0d9cfd
Wediah <100675006+Wediah@users.noreply.github.com>
2024-02-01 23:15:22 +0000

    Update @sanity/cli to version 3.27.0

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,15 +1,19 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-01-28'
 
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'production'
-)
+export const dataset = 
+  process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
+// export const dataset = assertValue(
+//   process.env.NEXT_PUBLIC_SANITY_DATASET,
+//   'production'
+// )
 
-export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'qe0yd7qt'
-)
+export const projectId = 
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'qe0yd7qt'
+// export const projectId = assertValue(
+//   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+//   'qe0yd7qt'
+// )
 
 export const useCdn = false
 
