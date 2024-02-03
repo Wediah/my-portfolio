86e1f2409fc3f5632a832282594651dd34b16193
Wediah <100675006+Wediah@users.noreply.github.com>
2024-02-03 23:21:36 +0000

    Update about page with focus on AI and ML

--- app/about/page.js
+++ app/about/page.js
@@ -24,7 +24,7 @@ function page() {
         Get to know me
       </h1>
       <p className='pt-5 w-9/12'>
-        Hello, I'm Emmanuel Wediah, a final year student at <a className='text-orange-500' href='https://atu.edu.gh/'>Accra Technical Universtity</a> studing Computer Science. Currently, my main focus is frontend web development as well as backend development with Laravel and PHP. <br/>
+        Hello, I'm Emmanuel Wediah, a final year student at <a className='text-orange-500' href='https://atu.edu.gh/'>Accra Technical Universtity</a> studing Computer Science. Currently, my main focus is frontend web development as well as backend development with Laravel and PHP. I look to becoming an expect in artificial intelligence and machine learning in the near future. <br/>
         <br/> I'm also a member of the <a className='text-orange-500' href='https://gdsc.community.dev/accra-technical-university/'>Google Developers Student Club (GDSC)</a> at my school, where I help organize events and workshops for students to learn about Google technologies.<br/>
          <br/> I hope to learn more about artificial intelligence and machine learning in the future. Aside from programming, I enjoy <a className='text-orange-500' href='/jpegs'>photography</a> and <a className='text-orange-500' href='https://open.spotify.com/show/05qwLlxgmpwf2R9mvkuNSp?si=d93ea9532ed24c3d'>editing podcasts</a>.  
 
