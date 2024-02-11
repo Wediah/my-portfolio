0be9764db0462104ef5580c895e85d74ecce3adf
Wediah <100675006+Wediah@users.noreply.github.com>
2024-02-11 23:31:24 +0000

    Update blog page heading

--- app/blog/page.js
+++ app/blog/page.js
@@ -6,7 +6,7 @@ export default function page() {
     <main className="flex min-h-screen px-4 md:px-32 container mx-auto ">
       <div className="md:pt-32 pt-28">
         <button className="flex items-center gap-1 bg-green-600 rounded-md p-1 pb-1"></button>
-        <h1>No articles yet, but watch this space for something great!</h1>
+        <h1>No articles yet, but watch this space for something great...</h1>
       </div>
     </main>
   );
