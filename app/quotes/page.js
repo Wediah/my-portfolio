38839c021aa031f4608b960b06d93f947f42e9c6
Wediah <100675006+Wediah@users.noreply.github.com>
2024-02-14 22:46:00 +0000

    Add famous quotes page

---
+++ app/quotes/page.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const quotes = [
+  {
+    author: 'Albert Einstein',
+    quote: 'Imagination is more important than knowledge.',
+  },
+  {
+    author: 'Steve Jobs',
+    quote: 'Stay hungry, stay foolish.',
+  },
+  {
+    author: 'Nelson Mandela',
+    quote: 'It always seems impossible until it\'s done.',
+  },
+];
+
+const QuotePage = () => {
+  return (
+    <div className="min-h-screen px-4 md:px-32 container mx-auto">
+      <h1>Famous Quotes</h1>
+      <div>
+        {quotes.map((quote, index) => (
+            <div className='flex bg-gray-700' key={index}>
+            <h3>{quote.author}</h3>
+            <p>{quote.quote}</p>
+            </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default QuotePage;
