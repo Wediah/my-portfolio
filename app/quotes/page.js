b82fcab3e79977e5b087d55099f3d51cb52a0a93
Wediah <100675006+Wediah@users.noreply.github.com>
2024-02-14 22:50:49 +0000

    Update quote page layout and content

--- app/quotes/page.js
+++ app/quotes/page.js
@@ -18,12 +18,13 @@ const quotes = [
 const QuotePage = () => {
   return (
     <div className="min-h-screen px-4 md:px-32 container mx-auto">
-      <h1>Famous Quotes</h1>
-      <div className='flex flex-col gap-2'>
+      <h1>These are some words from people, I deem wise...</h1>
+      <div className='flex flex-col gap-4 pt-4'>
         {quotes.map((quote, index) => (
-            <div className='flex flex-col bg-gray-700 rounded-md p-10' key={index}>
-            <h3>{quote.author}</h3>
+            <div className='flex flex-col  rounded-md p-10' key={index}>
+            <h3 cl>{quote.author}</h3>
             <p>{quote.quote}</p>
+            <hr className=''/>
             </div>
         ))}
       </div>
