1ba29a8408c6911b28e89ac189574662c58aa08e
Wediah <100675006+Wediah@users.noreply.github.com>
2024-05-13 19:53:13 +0000

    updates cdn values

--- sanity/env.ts
+++ sanity/env.ts
@@ -15,7 +15,7 @@ export const projectId =
 //   'qe0yd7qt'
 // )
 
-export const useCdn = true
+export const useCdn = false
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -6,5 +6,5 @@ export const client = createClient({
   apiVersion: '2024-01-28', // use a specific API version, or use 'v2021-03-25' for the latest
   dataset: 'production',
   projectId: 'qe0yd7qt',
-  useCdn: true, // `false` if you want to ensure fresh data
+  useCdn: false, // `false` if you want to ensure fresh data
 })
